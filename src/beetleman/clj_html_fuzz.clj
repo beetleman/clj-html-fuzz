3b96f3bb34e9f86bb8fcc6d51f3a75a53bbfaaa0
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2025-05-15 23:36:56 +0200

    init

---
+++ src/beetleman/clj_html_fuzz.clj
@@ -0,0 +1,12 @@
+(ns beetleman.clj-html-fuzz
+  "FIXME: my new org.corfield.new/scratch project.")
+
+(defn exec
+  "Invoke me with clojure -X beetleman.clj-html-fuzz/exec"
+  [opts]
+  (println "exec with" opts))
+
+(defn -main
+  "Invoke me with clojure -M -m beetleman.clj-html-fuzz"
+  [& args]
+  (println "-main with" args))
